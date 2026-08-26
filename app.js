78a5d521e3245f8d9a340aaa2f79a882fd2a134f
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2026-08-26 13:50:34 +0700

    update

--- app.js
+++ app.js
@@ -15,14 +15,6 @@ app.use(cors());
 // SERVE STATIC FILES - INI YANG DITAMBAHKAN!
 // Ini akan membuat file di folder uploads bisa diakses via URL
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// Atau bisa juga dengan:
-// app.use(express.static('uploads'));
-
-// Atau untuk mengizinkan origin tertentu
-// app.use(cors({
-//   origin: 'http://localhost:5173', // Frontend origin
-//   // credentials: true // Jika menggunakan cookies/auth
-// }));
 
 app.use(routes);
 
